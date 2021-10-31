854c91b615dda27ef5c6089701432bf8cb770ce1
Gilson Russo <gilsonrusso@outlook.com>
2021-10-30 20:13:18 -0400

    create user router

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 require('dotenv').config()
 import express from "express";
+import { router } from "./routes";
+import "reflect-metadata"
+import "./database";
 
 const app = express();
-
-app.get("/", (request, response) => {
-  return response.status(200).send();
-});
+app.use(express.json())
+app.use(router)
 
 export { app };
--- src/database/migrations/index.ts
+++
@@ -1,4 +0,0 @@
-import { createConnection } from "typeorm";
-
-// this connection will search the ormconfig file 
-createConnection();
